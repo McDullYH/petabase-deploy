faad1a6e5f920f92c377da812d3d361f55b482c2
root <root@last-233.(none)>
2015-05-18 18:37:09 +0800

    修正了函数，现在都是带参数调用，而非使用全局变量，为的是给config脚本复用

--- xml-handle.py
+++ xml-handle.py
@@ -22,6 +22,8 @@ import re
 #<value>true</value>
 #</configuration>
 
+# remember that print will output a var for shell, so all info will output in stderr
+
 NAME='name'
 VALUE='value'
 
@@ -40,14 +42,13 @@ class XMLConfigurator():
 
     def getProperty(self,name):
         if type(name)!=str:
-            print 'key must be str'
+            print_error('key must be str')
             return False
         ele=self.soup.find(name=NAME,text=name)
         if ele==None:
             return None
         else:
             #print "get succed it"
-            
             # text and string is all OK, but text is used by BS4, string is used by you
             # return ele.parent.find(VALUE).text
             return ele.parent.find(VALUE).string
@@ -55,7 +56,7 @@ class XMLConfigurator():
     # if has the property, change it, else create it
     def setProperty(self,name,value):
         if type(name)!=str or type(value)!=str:
-            print 'key and value must be str'
+            print_error('key and value must be str')
             return False
         if not verify(name,value):
             return False
@@ -129,7 +130,7 @@ def verify(name,value):
     if verifitor.get(name.strip())==None:
         return True
     if not verifitor[name.strip()](value.strip()):
-        print "property '%s' is not valid for value '%s' "  % (name,value)
+        print("property '%s' is not valid for value '%s' "  % (name,value) )
     else:
         return True
     
@@ -156,6 +157,140 @@ MAPRED_XML="mapred-site.xml"
 HIVE_XML="hive-site.xml"
 ZOO_CFG="zoo.cfg"
 
+HOST_INFO_XML="host-info.xml"
+
+
+src_configure_dir=None
+dest_configure_dir=None
+
+namenode=None
+datanode_list=None
+
+# now no use, later may use
+second_namenode=None
+mysql_root_passwd=None
+
+
+# 这样处理应该有设计上的问题,所有的配置文件就一个
+class NameNode:
+    def __init__(self):
+        self.hdfs_configure=XMLConfigurator(src_configure_dir+HADOOP_DIR+NAMENODE_DIR+HDFS_XML)
+        self.mapred_configure=XMLConfigurator(src_configure_dir+HADOOP_DIR+NAMENODE_DIR+MAPRED_XML)
+        self.core_configure=XMLConfigurator(src_configure_dir+HADOOP_DIR+NAMENODE_DIR+CORE_XML)
+    def save(self):
+        self.hdfs_configure.write(dest_configure_dir+HADOOP_DIR+NAMENODE_DIR+HDFS_XML)
+        self.mapred_configure.write(dest_configure_dir+HADOOP_DIR+NAMENODE_DIR+MAPRED_XML)
+        self.core_configure.write(dest_configure_dir+HADOOP_DIR+NAMENODE_DIR+CORE_XML)
+
+
+class DataNode:
+    def __init__(self):
+        self.hdfs_configure=XMLConfigurator(src_configure_dir +  HADOOP_DIR +  DATANODE_DIR + HDFS_XML)
+        self.mapred_configure=XMLConfigurator(src_configure_dir +  HADOOP_DIR + DATANODE_DIR + MAPRED_XML)
+        self.core_configure=XMLConfigurator(src_configure_dir +  HADOOP_DIR + DATANODE_DIR + CORE_XML)
+    def save(self):
+        self.mapred_configure.write(dest_configure_dir + HADOOP_DIR + DATANODE_DIR + MAPRED_XML)
+        self.core_configure.write(dest_configure_dir + HADOOP_DIR + DATANODE_DIR + CORE_XML)
+        self.hdfs_configure.write(dest_configure_dir + HADOOP_DIR + DATANODE_DIR + HDFS_XML)
+
+class Petabase:
+    def __init__(self):
+        self.hdfs_configure=XMLConfigurator(src_configure_dir + PETABASE_DIR + HDFS_XML)
+        self.core_configure=XMLConfigurator(src_configure_dir + PETABASE_DIR + CORE_XML)
+        self.hive_configure=XMLConfigurator(src_configure_dir + PETABASE_DIR + HIVE_XML)
+    def save(self):
+        self.hdfs_configure.write(dest_configure_dir + PETABASE_DIR + HDFS_XML)
+        self.hive_configure.write(dest_configure_dir + PETABASE_DIR + HIVE_XML)
+        self.core_configure.write(dest_configure_dir + PETABASE_DIR + CORE_XML)
+
+class Hive:
+    def __init__(self):
+        self.hive_configure=XMLConfigurator(src_configure_dir + HIVE_DIR + HIVE_XML)
+    def save(self):
+        self.hive_configure.write(dest_configure_dir + HIVE_DIR + HIVE_XML)
+        
+
+class HostInfo:
+    def __init__(self):
+        self.host_info_configure=XMLConfigurator(HOST_INFO_XML)
+    def getProperty(self,name):
+        return self.host_info_configure.getProperty(name)
+    def setProperty(self,name,value):
+        return self.host_info_configure.setProperty(name,value)
+    def save(self):
+        self.host_info_configure.write(HOST_INFO_XML)
+
+class ZooKeeper:
+    src_file=None
+    dest_file=None
+    def open(self):
+        self.src_file=open(src_configure_dir + ZOOKEEPER_DIR + ZOO_CFG,"r")
+        self.dest_file=open(dest_configure_dir + ZOOKEEPER_DIR + ZOO_CFG,"w")
+    def save(self):
+        self.dest_file.write(self.src_file.read());
+        i=1
+        self.dest_file.write("server.%d=%s:2888:3888\n" % (i,namenode))
+        i=i+1
+        for datanode in datanode_list.split(','):
+            self.dest_file.write("server.%d=%s:2888:3888\n" % (i,datanode))
+            i=i+1
+        self.src_file.close()
+        self.dest_file.close()
+
+
+
+def init_host_config():
+    nn=NameNode()
+    nn.hdfs_configure.setProperty(r'dfs.namenode.http-address',"%s:50070" % (namenode))
+    nn.mapred_configure.setProperty(r'mapred.job.tracker',"%s:8021" % (namenode))
+    nn.core_configure.setProperty(r'fs.defaultFS',"hdfs://%s:8020" % (namenode))
+
+    dn=DataNode()
+    dn.mapred_configure.setProperty(r'mapred.job.tracker',"%s:8021" % (namenode))
+    dn.core_configure.setProperty(r'fs.defaultFS',"hdfs://%s:8020" % (namenode))
+
+    pb=Petabase()
+    pb.hdfs_configure.setProperty(r'dfs.namenode.http-address',"%s:50070" % (namenode))
+    pb.hive_configure.setProperty(r'javax.jdo.option.ConnectionURL',
+        "jdbc:mysql://%s:3306/hive_metastore?createDatabaseIfNotExist=true&amp;characterEncoding=utf8&amp;useUnicode=true" % (namenode))
+    pb.hive_configure.setProperty(r'hive.metastore.uris',"thrift://%s:9083" % (namenode))
+    pb.hive_configure.setProperty(r'hive.zookeeper.quorum',"%s,%s" % (namenode,datanode_list))
+    pb.core_configure.setProperty(r'fs.defaultFS',"hdfs://%s:8020" % (namenode))
+
+    h=Hive()
+    h.hive_configure.setProperty(r'javax.jdo.option.ConnectionURL',
+        "jdbc:mysql://%s:3306/hive_metastore?createDatabaseIfNotExist=true&amp;characterEncoding=utf8&amp;useUnicode=true" % (namenode))
+    h.hive_configure.setProperty(r'hive.metastore.uris',"thrift://%s:9083" % (namenode))
+    h.hive_configure.setProperty(r'hive.zookeeper.quorum',"%s,%s" % (namenode,datanode_list))
+    h.hive_configure.setProperty(r'javax.jdo.option.ConnectionPassword',"%s" % (mysql_root_passwd))
+
+    zk=ZooKeeper()
+    zk.open()
+
+    nn.save()
+    dn.save()
+    h.save()
+    pb.save()
+    zk.save()
+
+# 升级(改变)配置，从ripe中读，写到tmp，然后写回ripe（直接调用cp操作即可），由bash进行后续的cp操作
+def update_host_config(): 
+    pass
+
+def updete_host_info():
+    pass
+
+def init_host_info():
+    pass
+
+def get_host_info(name):
+    hi=HostInfo()
+    return hi.getProperty(name)
+
+def set_host_info(name,value):
+    hi=HostInfo()
+    hi.setProperty(name,value)
+    hi.save()
 
 if __name__=='__main__':
     #c=XMLConfigurator(sys.argv[1])
@@ -176,8 +311,6 @@ if __name__=='__main__':
     #c.write()
     #print c.getProperty(r'dfs.namenode.name.dir')
 
-    src_configure_dir=None
-    dest_configure_dir=None
 
 
     # 这里由于都是自己调用这个脚本，所以顺序自己清楚
@@ -185,124 +318,32 @@ if __name__=='__main__':
     if sys.argv[1] == 'init':
         src_configure_dir='configuration.raw/'
         dest_configure_dir='configuration.ripe/'
+        namenode=sys.argv[2]
+        datanode_list=sys.argv[3]
+        # now no use, later may use
+        second_namenode=sys.argv[4]
+        mysql_root_passwd=sys.argv[5]
+        init_host_info()
+        init_host_config()
     elif sys.argv[1] =='update':
         src_configure_dir='configuration.ripe/'
         dest_configure_dir='configuration.tmp/'
+        namenode=sys.argv[2]
+        datanode_list=sys.argv[3]
+        # now no use, later may use
+        second_namenode=sys.argv[4]
+        mysql_root_passwd=sys.argv[5]
+        update_host_info()
+        update_host_config()
+    elif sys.argv[1] == 'get':
+        property_name=sys.argv[2]
+        property_value=get_host_info(property_name)
+        print property_value
+        # can be done in init host info  and update host info
+    elif sys.argv[1] == 'set':
+        property_name=sys.argv[2]
+        property_value=sys.argv[3]
+        set_host_info(property_name,property_value)
     else:
-        print "bad operator"
-
-    namenode=sys.argv[2]
-    datanode_list=sys.argv[3]
-
-    # now no use, later may use
-    second_namenode=sys.argv[4]
-    mysql_root_passwd=sys.argv[5]
-
-    class NameNode:
-        hdfs_configure=XMLConfigurator(src_configure_dir+HADOOP_DIR+NAMENODE_DIR+HDFS_XML)
-        mapred_configure=XMLConfigurator(src_configure_dir+HADOOP_DIR+NAMENODE_DIR+MAPRED_XML)
-        core_configure=XMLConfigurator(src_configure_dir+HADOOP_DIR+NAMENODE_DIR+CORE_XML)
-        @classmethod
-        def save(cls):
-            cls.hdfs_configure.write(dest_configure_dir+HADOOP_DIR+NAMENODE_DIR+HDFS_XML)
-            cls.mapred_configure.write(dest_configure_dir+HADOOP_DIR+NAMENODE_DIR+MAPRED_XML)
-            cls.core_configure.write(dest_configure_dir+HADOOP_DIR+NAMENODE_DIR+CORE_XML)
-
-    
-    class DataNode:
-        hdfs_configure=XMLConfigurator(src_configure_dir +  HADOOP_DIR +  DATANODE_DIR + HDFS_XML)
-        mapred_configure=XMLConfigurator(src_configure_dir +  HADOOP_DIR + DATANODE_DIR + MAPRED_XML)
-        core_configure=XMLConfigurator(src_configure_dir +  HADOOP_DIR + DATANODE_DIR + CORE_XML)
-        @classmethod
-        def save(cls):
-            cls.mapred_configure.write(dest_configure_dir + HADOOP_DIR + DATANODE_DIR + MAPRED_XML)
-            cls.core_configure.write(dest_configure_dir + HADOOP_DIR + DATANODE_DIR + CORE_XML)
-            cls.hdfs_configure.write(dest_configure_dir + HADOOP_DIR + DATANODE_DIR + HDFS_XML)
-    
-    class Petabase:
-        hdfs_configure=XMLConfigurator(src_configure_dir + PETABASE_DIR + HDFS_XML)
-        core_configure=XMLConfigurator(src_configure_dir + PETABASE_DIR + CORE_XML)
-        hive_configure=XMLConfigurator(src_configure_dir + PETABASE_DIR + HIVE_XML)
-        @classmethod
-        def save(cls):
-            cls.hdfs_configure.write(dest_configure_dir + PETABASE_DIR + HDFS_XML)
-            cls.hive_configure.write(dest_configure_dir + PETABASE_DIR + HIVE_XML)
-            cls.core_configure.write(dest_configure_dir + PETABASE_DIR + CORE_XML)
-    
-    class Hive:
-        hive_configure=XMLConfigurator(src_configure_dir + HIVE_DIR + HIVE_XML)
-        @classmethod
-        def save(cls):
-            cls.hive_configure.write(dest_configure_dir + HIVE_DIR + HIVE_XML)
-            
-
-    class ZooKeeper:
-        src_file=None
-        dest_file=None
-        @classmethod
-        def open(cls):
-            cls.src_file=open(src_configure_dir + ZOOKEEPER_DIR + ZOO_CFG,"r")
-            cls.dest_file=open(dest_configure_dir + ZOOKEEPER_DIR + ZOO_CFG,"w")
-        @classmethod
-        def save(cls):
-            cls.dest_file.write(cls.src_file.read());
-            i=1
-            cls.dest_file.write("server.%d=%s:2888:3888\n" % (i,namenode))
-            i=i+1
-            for datanode in datanode_list.split(','):
-                cls.dest_file.write("server.%d=%s:2888:3888\n" % (i,datanode))
-                i=i+1
-            cls.src_file.close()
-            cls.dest_file.close()
-       
-
-    # 初始化配置，从raw中读，写到ripe，由bash进行后续的cp操作
-    def init_cfg():       
-        NameNode.hdfs_configure.setProperty(r'dfs.namenode.http-address',"%s:50070" % (namenode))
-        NameNode.mapred_configure.setProperty(r'mapred.job.tracker',"%s:8021" % (namenode))
-        NameNode.core_configure.setProperty(r'fs.defaultFS',"hdfs://%s:8020" % (namenode))
-
-        DataNode.mapred_configure.setProperty(r'mapred.job.tracker',"%s:8021" % (namenode))
-        DataNode.core_configure.setProperty(r'fs.defaultFS',"hdfs://%s:8020" % (namenode))
-
-        Petabase.hdfs_configure.setProperty(r'dfs.namenode.http-address',"%s:50070" % (namenode))
-        Petabase.hive_configure.setProperty(r'javax.jdo.option.ConnectionURL',
-            "jdbc:mysql://%s:3306/hive_metastore?createDatabaseIfNotExist=true&amp;characterEncoding=utf8&amp;useUnicode=true" % (namenode))
-        Petabase.hive_configure.setProperty(r'hive.metastore.uris',"thrift://%s:9083" % (namenode))
-        Petabase.hive_configure.setProperty(r'hive.zookeeper.quorum',"%s,%s" % (namenode,datanode_list))
-        Petabase.core_configure.setProperty(r'fs.defaultFS',"hdfs://%s:8020" % (namenode))
-
-        Hive.hive_configure.setProperty(r'javax.jdo.option.ConnectionURL',
-            "jdbc:mysql://%s:3306/hive_metastore?createDatabaseIfNotExist=true&amp;characterEncoding=utf8&amp;useUnicode=true" % (namenode))
-        Hive.hive_configure.setProperty(r'hive.metastore.uris',"thrift://%s:9083" % (namenode))
-        Hive.hive_configure.setProperty(r'hive.zookeeper.quorum',"%s,%s" % (namenode,datanode_list))
-        Hive.hive_configure.setProperty(r'javax.jdo.option.ConnectionPassword',"%s" % (mysql_root_passwd))
-
-
-        NameNode.save()
-        DataNode.save()
-        Hive.save()
-        Petabase.save()
+        print_error("bad operator")
         
-        ZooKeeper.open()
-        ZooKeeper.save()
-
-    # 升级(改变)配置，从ripe中读，写到tmp，然后写回ripe（直接调用cp操作即可），由bash进行后续的cp操作
-    def update_cfg(): 
-        pass
-
-
-
-    if sys.argv[1] == 'init':
-        init_cfg()
-    elif sys.argv[1] =='update':
-        update_cfg()
-    else:
-        # TODO add later
-        pass
-
-
-
-
-
-
