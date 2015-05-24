76612a82fc8f1634bf01aacb17a398d1956895fe
root <root@petabase-233.(none)>
2015-05-25 05:17:58 +0800

    新增ntp服务的设置来同步集群时间 start/stop服务的处理更为合理 精简代码，细节改进

--- utils.py
+++ utils.py
@@ -9,8 +9,21 @@ if __name__=="__main__":
         b=set(sys.argv[3].split(','))
         intersection= set(a) & set(b) 
         sys.exit(len(intersection))
+
     elif sys.argv[1]=="test_subset":
         big=set(sys.argv[2].split(','))
         small=set(sys.argv[3].split(','))
         sys.exit(big >= small)
+
+    elif sys.argv[1]=="get_start_addr":
+        ip_l=sys.argv[2].split('.')
+        mask_l=sys.argv[3].split('.')
+        start_l=[ str (int(ip_l[0]) & int(mask_l[0])),
+         str (int(ip_l[1]) & int(mask_l[1])),
+         str (int(ip_l[2]) & int(mask_l[2])),
+         str (int(ip_l[3]) & int(mask_l[3])),
+        ]
+        start='.'.join(start_l)
+        #print "restrict " + start + " mask " + sys.argv[3] +" nomodify"
+        print start
 
